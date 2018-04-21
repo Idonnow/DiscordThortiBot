e89cf21798ca425714240ad4bd59a71fd16eeaef
Camille poumaredes <camille.poumaredes@gmail.com>
2018-04-21 12:44:39 +0200

    seems work

--- bot.js
+++ bot.js
@@ -1,10 +1,14 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-var quizz = require('./quizz.json')
+var quizz = require('./quizz.json');
+var timer = require('timers');
 var maxQuestion = 9;
 var players = new Array();
+var playersScore = new Array();
 var playersNb = 0;
+var bugpasstp = '';
+var flagQ = false;
 
 
 // Configure logger settings
@@ -44,59 +48,90 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'help' :
               help(channelID);
             break;
-            case 'quizz':
-              quiz(channelID);
+            case 'moi':
+              participe(channelID,user);
             break;
-            case 'question':
-                var questionNb = Math.round(Math.random()*(maxQuestion-1));
-                answer = quizz.quizz[questionNb].A;
-                bot.sendMessage({
-                    to: channelID,
-                    message: quizz.quizz[questionNb].Q
-                });
+            case 'score':
+              score(channelID);
+            break;
+            case 'run':
+              run(channelID);
+            break;
 
-            break;
             // Just add any case commands if you want to..
          }
      }
+     else answerVerif(channelID,user,message);
+
 });
 
-function quiz(channelID)
+function run (channelID)
 {
-  bot.sendMessage({
-      to: channelID,
-      message: 'Qui veux jouer ? Veuillez répondre !moi. Vous avez 30" pour vous inscrire'
-  });
-  setTimeout(bot.on('message', function (user, userID, channelID, message, evt) {
-      // Our bot needs to know if it will execute a command
-      // It will listen for messages that will start with `!`
-      if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-        args = args.splice(1);
-        var answer = "";
-        switch(cmd) {
-          case 'moi' :
-            players[playersNb] = user;
-          break;
-        }
-      }
-    }), 30000);
+  if (playersNb<2)
+  {
     bot.sendMessage({
         to: channelID,
-        message: 'Les inscriptions sont terminées. Les joueurs sont : '
+        message: 'Pas assez de joueurs (min 2)'
     });
-    for (i = 0; i < playersNb; i++)
+  }
+  else
     {
+      var questionNb = Math.round(Math.random()*(maxQuestion-1));
+      bugpasstp = quizz.quizz[questionNb].A;
       bot.sendMessage({
           to: channelID,
-          message: players[i]
+          message: 'Question : ' + quizz.quizz[questionNb].Q
       });
-      i++;
-      i--;
+      logger.info ('Q: ' + quizz.quizz[questionNb].Q + '\nR: '+quizz.quizz[questionNb].A);
+      flagQ = true;
     }
 }
 
+function answerVerif (channelID,user,message)
+{
+  //logger.info('message : '+ message, ' , answer ! ' + bugpasstp);
+  //if (strcmp(message,bugpasstp) == true)
+  if (message == bugpasstp)
+  {
+    if (flagQ == true)
+    {
+      var index = players.indexOf(user);
+      if (index  != -1)
+      {
+        bot.sendMessage({
+            to: channelID,
+            message: 'Bravo @' + user + '! la réponses était ' + bugpasstp
+        });
+        playersScore[index]++;
+        flagQ = false;
+      }
+    }
+  }
+}
+
+function score(channelID)
+{
+  var msg = 'Tableau des scores :\n';
+  for (var i=0; i<playersNb; i++)
+  {
+    msg = msg + players[i] + ' : '  + playersScore[i] + '\n';
+  }
+  bot.sendMessage({
+      to: channelID,
+      message: msg
+  });
+}
+
+function participe(channelID,user)
+{
+  players.push(user);
+  playersScore.push(0);
+  playersNb++;
+  bot.sendMessage({
+      to: channelID,
+      message: user + ' participe au tournois'
+  });
+}
 
 function pong(channelID)
 {
@@ -113,15 +148,21 @@ function help(channelID)
   txt[1] = 'Ci dessous la liste des commandes utilisable';
   txt[2] = '- !ping : répond pong';
   txt[3] = '- !help : ce que tu viens de taper';
-  txt[4] = '= !quizz lance le quizz'
-  txt[5] = 'en construction';
-  for (i = 0; i < 6; i++)
+  txt[4] = '- !moi : t ajoute à la liste des joueurs'
+  txt[5] = '- !score : donne les scores'
+  txt[6] = '- !run : pose une question';
+  var msg = '';
+  for (i = 0; i < 7; i++)
   {
-    bot.sendMessage({
-        to: channelID,
-        message: txt[i]
-    });
-    i++;
-    i--;
+    msg = msg +  txt[i] + '\n';
   }
+  bot.sendMessage({
+      to: channelID,
+      message: msg
+  });
+}
+
+function strcmp(a, b)
+{
+    return (a<b?-1:(a>b?1:0));
 }
